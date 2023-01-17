f47bd948cd15d873ab9d131ba6355835c5bb72ae
hemapriya <hemapriyasampath@gmail.com>
2023-01-17 12:59:36 -0500

    adding controller

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,58 +1,15 @@
 const express = require('express');
-const Blog = require('../models/blogSchema');
+
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
 
-  router.get('/create',(req,res)=>{
-    res.render('create',{ title:'Create a new blog' })
-  })
-
-  //blog routes
-  router.get('/',(req,res)=>{
-    Blog.find()
-    .then((result)=>{
-      //console.log(result.length);
-      //console.log(result);
-      res.render('index',{title:'All Blogs Test',blogs:result})
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  });
-
-  router.get('/:id',(req,res)=>{
-    const id = req.params.id;
-    console.log("id is=============>"+id);
-    Blog.findById(id)
-      .then((result) =>{
-        console.log(result);
-        res.render('details',{blog:result,title:'Blog Details'});
-      })
-      .catch((err)=> {
-        console.log(err);
-      });
-  })
-
-  router.delete('/:id',(req,res)=>{
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-      .then((result)=>{
-        res.json({redirect:'/blogs'});
-      })
-       .catch((err)=>{console.log(err)});
-      })
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 
-  router.post('/',(req,res)=>{
-    const blog= new Blog(req.body);
-      blog.save()
-        .then((result)=>{
-          res.redirect('/blogs');
-        })
-        .catch((err)=>{
-          console.log(err);
-        })
-  })
-
-  module.exports = router;
+module.exports = router;
